23f84cb37583784e1abddaeb16601a9c5b7b9e24
gureenkov56 <gureenkov56@gmail.com>
2023-05-29 00:05:35 +0500

    first commit

---
+++ components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        TEST connection: {{ connection }}
+    </div>
+</template>
+
+<script lang=ts>
+import { defineComponent, ref, onMounted } from 'vue'
+
+export default defineComponent({
+    name: 'Test',
+    setup() {
+        const connection = ref<string>('NO CONNECTION');
+
+        onMounted(() => {
+            console.log('mounted');
+            fetch('/api/test')
+                .then(res => res.json())
+                .then(res => connection.value = res.message)
+        })
+
+        return {
+            connection
+        }
+    }
+})
+</script>
